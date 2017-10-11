787bbe42ededfb0137a761a360995b69ae8acd63
Robin Bisping <robin@bisping.ch>
2017-10-11 10:01:45 +0200

    Add configuration files to app and connection to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+// Load configuration
+const config = require('./config/config');
+
+// Connect to database
+const mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost/neon';
+mongoose.connect(config.database.mongodb.url + config.database.mongodb.name, {
+    user: config.database.mongodb.user,
+    pass: config.database.mongodb.password,
+    useMongoClient: true
+});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // Routes
 const web = require('./routes/web');
 const api = require('./routes/api');
