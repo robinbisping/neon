2e978a4a6893d38bcee92a90596f7e531772f737
Robin Bisping <robin@bisping.ch>
2017-10-19 17:31:23 +0200

    Modify user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,41 +1,47 @@
-const User = require('../models/user');
+const User = require('../models/user')
 
 module.exports = {
+
 	list: function(req, res) {
 		User.find(function(err, users){
-			if(err) {
-				return res.status(500).json({
-					message: 'err getting users.'
-				});
-			}
-			return res.json(users);
+			if(err)
+				return res.send(err)
+			res.json(users);
+		})
+	},
+
+	get: function(req, res) {
+		var id = req.params.id
+		User.findById(id, function(err, user){
+			if(err)
+				res.send(err)
+			res.json(user)
 		});
 	},
-	show: function(req, res) {
-		var id = req.params.id;
-		User.findOne({_id: id}, function(err, user){
-			if(err) {
-				return res.status(500).json({
-					message: 'err getting user.'
-				});
-			}
-			if(!user) {
-				return res.status(404).json({
-					message: 'No such user.'
-				});
-			}
-			return res.json(user);
-		});
+
+	create: function(req, res) {
+		var user = new User({
+			email: req.body.email,
+			password: req.body.password,
+		})
+		user.save(function(err){
+			if(err)
+				res.send(err)
+			res.json({
+				message: 'User created.',
+			})
+		})
 	},
+
 	remove: function(req, res) {
-		var id = req.params.id;
-		User.findByIdAndRemove(id, function(err, user){
-			if(err) {
-				return res.status(500).json({
-					message: 'err getting user.'
-				});
-			}
-			return res.json(user);
-		});
+		var id = req.params.id
+		User.findByIdAndRemove(id, function(err){
+			if(err)
+				res.send(err)
+			res.json({
+				message: 'User removed.',
+			})
+		})
 	},
-};
+
+}
