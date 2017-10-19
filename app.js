77e2a2b9ed45cbaeea0179f9e8f9da691a086ad2
Robin Bisping <robin@bisping.ch>
2017-10-19 17:43:38 +0200

    Add error handler

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // Load configuration
-const config = require('./config/config');
+const config = require('./config/config')
 
 // Connect to database
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 mongoose.connect(config.db.mongo.url + config.db.mongo.name, {
 	user: config.db.mongo.user,
 	pass: config.db.mongo.password,
-	useMongoClient: true
-});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+	useMongoClient: true,
+})
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // Use passport for authentication
-const passport = require('passport');
-const authController = require('./controllers/auth');
-app.use(passport.initialize());
+const passport = require('passport')
+const authController = require('./controllers/auth')
+app.use(passport.initialize())
 
 // Routes
-const web = require('./routes/web');
-const api = require('./routes/api');
-app.use('/', web);
-app.use('/api', authController.is_authenticated, api);
+const web = require('./routes/web')
+const api = require('./routes/api')
+app.use('/', web)
+app.use('/api', authController.is_authenticated, api)
 
-module.exports = app;
+// Error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500)
+	res.json(err)
+})
+
+module.exports = app
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,42 +2,42 @@ const User = require('../models/user')
 
 module.exports = {
 
-	list: function(req, res) {
+	list: function(req, res, next) {
 		User.find(function(err, users){
 			if(err)
-				return res.send(err)
+				return next(err)
 			res.json(users);
 		})
 	},
 
-	get: function(req, res) {
+	get: function(req, res, next) {
 		var id = req.params.id
 		User.findById(id, function(err, user){
 			if(err)
-				res.send(err)
+				return next(err)
 			res.json(user)
 		});
 	},
 
-	create: function(req, res) {
+	create: function(req, res, next) {
 		var user = new User({
 			email: req.body.email,
 			password: req.body.password,
 		})
 		user.save(function(err){
 			if(err)
-				res.send(err)
+				return next(err)
 			res.json({
 				message: 'User created.',
 			})
 		})
 	},
 
-	remove: function(req, res) {
+	remove: function(req, res, next) {
 		var id = req.params.id
 		User.findByIdAndRemove(id, function(err){
 			if(err)
-				res.send(err)
+				return next(err)
 			res.json({
 				message: 'User removed.',
 			})
