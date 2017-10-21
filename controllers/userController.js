925913d8794e2772b449f421db1906d02733427a
Robin Bisping <robin@bisping.ch>
2017-10-21 18:12:01 +0200

    Add update method to user controller and improve error messages with boom

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,26 @@
+const boom = require('boom')
+
 const User = require('../models/user')
 
 module.exports = {
 
 	list: function(req, res, next) {
-		User.find(function(err, users){
+		User.find(function(err, users) {
 			if(err)
 				return next(err)
 			res.json(users);
 		})
 	},
 
-	get: function(req, res, next) {
+	read: function(req, res, next) {
 		var id = req.params.id
-		if(!id.match(/^[0-9a-fA-F]{24}$/)) {
-			return res.status(404).json({
-				error: {
-					status: 404,
-					data: req.params,
-					message: 'User does not exist.',
-				},
-			})
-		}
-		User.findById(id, function(err, user){
+		if(!id.match(/^[0-9a-fA-F]{24}$/))
+			return next(boom.notFound('User ID invalid.'))
+		User.findById(id, function(err, user) {
 			if(err)
 				return next(err)
-			if(!user) {
-				return res.status(404).json({
-					error: {
-						status: 404,
-						data: req.params,
-						message: 'User does not exist.',
-					},
-				})
-			}
+			if(!user)
+				return next(boom.notFound('User not found.'))
 			res.json(user)
 		})
 	},
@@ -43,23 +31,46 @@ module.exports = {
 			password: req.body.password,
 			registered: Date.now(),
 		})
-		user.save(function(err){
+		user.save(function(err) {
 			if(err)
 				return next(err)
-			res.json({
-				message: 'User created.',
+			user = user.toObject()
+			delete user.password
+			res.status(201).json({
+				data: user,
+			})
+		})
+	},
+
+	update: function(req, res, next) {
+		var id = req.params.id
+		if(!id.match(/^[0-9a-fA-F]{24}$/))
+			return next(boom.notFound('User ID invalid.'))
+		User.findById(id).select('+password').exec(function(err, user) {
+			if(err)
+				return next(err)
+			if(!user)
+				return next(boom.notFound('User not found.'))
+			user.email = req.body.email || user.email
+			user.password = req.body.password || user.password
+			user.save(function(err) {
+				if(err)
+					return next(err)
+				user = user.toObject()
+				delete user.password
+				res.json({
+					data: user,
+				})
 			})
 		})
 	},
 
 	delete: function(req, res, next) {
 		var id = req.params.id
-		User.findByIdAndRemove(id, function(err){
+		User.findByIdAndRemove(id, function(err) {
 			if(err)
 				return next(err)
-			res.json({
-				message: 'User deleted.',
-			})
+			res.status(204)
 		})
 	},
 
