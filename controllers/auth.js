d6168856308080569df8567ed7c12cc60c6604a9
Robin Bisping <robin@bisping.ch>
2017-10-22 14:37:44 +0200

    Create auth controller for login and register functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,25 @@
+const jwt = require('jsonwebtoken');
 
+const config = require('../config/app');
 
+exports.login = function (req, res, next) {
+	const token = jwt.sign({
+		id: req.user._id
+	}, config.auth.jwt.secret);
+	console.log(token);
+	res.json({
+		token: token
+	});
+};
+
+exports.register = function (req, res, next) {
+	const token = jwt.sing({
+		id: req.user._id
+	}, config.auth.jwt.secret, {
+		expiresIn: 10080
+	});
+	res.json({
+		token: 'JWT ' + token,
+		user: req.user
+	});
+};
