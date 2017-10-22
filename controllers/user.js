b7b65e0072399adecb1c1d1c438d87e12a7db363
Robin Bisping <robin@bisping.ch>
2017-10-22 14:33:29 +0200

    Rename user controller

---
+++ controllers/user.js
@@ -0,0 +1,79 @@
+const boom = require('boom');
+
+const User = require('../models/user');
+
+exports.read = function (req, res, next) {
+	var id = req.params.id;
+	if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+		return next(boom.notFound('User ID invalid.'));
+	}
+	User.findById(id, function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return next(boom.notFound('User not found.'));
+		}
+		res.json(user);
+	});
+};
+
+exports.create = function (req, res, next) {
+	let user = new User({
+		email: req.body.email,
+		password: req.body.password,
+		registered: Date.now()
+	});
+	user.save(function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		res.status(201).json({
+			data: user
+		});
+	});
+};
+
+exports.update = function (req, res, next) {
+	var id = req.params.id;
+	if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+		return next(boom.notFound('User ID invalid.'));
+	}
+	User.findById(id).select('+password').exec(function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return next(boom.notFound('User not found.'));
+		}
+		user.email = req.body.email || user.email;
+		user.password = req.body.password || user.password;
+		user.save(function (err, user) {
+			if (err) {
+				return next(err);
+			}
+			res.json({
+				data: user
+			});
+		});
+	});
+};
+
+exports.delete = function (req, res, next) {
+	var id = req.params.id;
+	User.findByIdAndRemove(id, function (err) {
+		if (err) {
+			return next(err);
+		}
+		res.status(204);
+	});
+};
+
+exports.list = function (req, res, next) {
+	User.find(function (err, users) {
+		if (err) {
+			return next(err);
+		}
+		res.json(users);
+	});
+};
